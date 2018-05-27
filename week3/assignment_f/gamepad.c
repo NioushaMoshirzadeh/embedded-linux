bde23f7a76deb238957bd953dbe3f3f6b842f959
joshbindels <josh.bindels@gmail.com>
2018-05-27 20:08:51 +0200

    Added sequences and triggers

--- week3/assignment_f/gamepad.c
+++ week3/assignment_f/gamepad.c
@@ -1,90 +1,104 @@
 #include <libusb-1.0/libusb.h>
 #include <stdio.h>
+#include "gamepad.h"
 
-unsigned char RUMBLE_HIGH[] = { 0, 8, 0, 0xff, 0xff, 0, 0, 0 };
-unsigned char RUMBLE_MID[] = { 0, 8, 0, 0x88, 0x88, 0, 0, 0 };
-unsigned char RUMBLE_LOW[] = { 0, 8, 0, 0x22, 0x22, 0, 0, 0 };
-unsigned char RUMBLE_OFF[] = { 0, 8, 0, 0, 0, 0, 0, 0 };
-unsigned char INPUT[256] = {0, 34, 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
+/*###################################################################*/
 
 int main(int argc, char *argv[])
 {
-	libusb_device_handle *h;
+	libusb_device_handle *h;        // usb device handle
 
+	libusb_init(NULL);              // init libusb library
 
-	libusb_init(NULL);
+    /* attempt to open xbox controller connection , exit on error */
 	h = libusb_open_device_with_vid_pid(NULL, 0x045e, 0x028e);
 	if (h == NULL) {
 		fprintf(stderr, "Failed to open device\n");
 		return (1);
 	}
 
+    /* 
+     * main program loop
+     */
     int error, transferred;
     int run = 1;
     while (run == 1)
     {
-        int cmd;
-        printf("Enter: ");
-        scanf("%d", &cmd);
-        switch(cmd)
-        {
-            case 0:
-                error = libusb_interrupt_transfer(
-                    h, 0x02, RUMBLE_OFF, sizeof RUMBLE_OFF, 
-                    &transferred, 0
-                );
-                break;
-            case 1:
-                error = libusb_interrupt_transfer(
-                    h, 0x02, RUMBLE_LOW, sizeof RUMBLE_LOW, 
-                    &transferred, 0
-                );
-                break;
-            case 2:
-                error = libusb_interrupt_transfer(
-                    h, 0x02, RUMBLE_MID, sizeof RUMBLE_MID, 
-                    &transferred, 0
-                );
-                break;
-            case 3:
-                error = libusb_interrupt_transfer(
-                    h, 0x02, RUMBLE_HIGH, sizeof RUMBLE_HIGH, 
-                    &transferred, 0
-                );
-                break;
-            case 4:
-                error = libusb_interrupt_transfer(
-                    h, 0x81, INPUT, sizeof(INPUT), &transferred, 0
-                );
-                printf("Transferred: %d\n", transferred);
-                switch (INPUT[2])
-                {
-                    case 1:
-                        printf("Up d-pad\n");
-                        break;
-                    case 2:
-                        printf("Down d-pad\n");
-                        break;
-                    case 4:
-                        printf("Left d-pad\n");
-                        break;
-                    case 8:
-                        printf("Right d-pad\n");
-                        break;
-                }
-                printf("\n");
-                break;
-            default:
-                run = 0;
-                break;
-        }
+        error = libusb_interrupt_transfer(
+            h, IN, INPUT, sizeof(INPUT), &transferred, 0
+        );
         if (error != 0)
         {
             fprintf(stderr, "Transfer failed: %d\n", error);
             error = 0;
+            continue;
         }
-        
+        ShowInput();
+        HandleTrigger(h, &trig_rmbl, &RUMBLE);
+        HandleTrigger(h, &trig_led, &LED);
     }
 
 	return (0);
+}
+
+/*###################################################################*/
+
+void HandleTrigger(libusb_device_handle* h, TRIGGER* trigger, 
+SEQUENCE* seq)
+{
+    int error, transferred;
+    /* button is pressed */
+    if ((INPUT[trigger->index] & (1<<trigger->bit)) != 0) 
+    {
+        if (trigger->active == 0)   // button isnt flagged as pressed 
+        {
+            error = libusb_interrupt_transfer(
+                h, OUT,
+                seq->cmds[seq->i],seq->cmd_len,
+                &transferred, 0
+            );
+            if (error != 0)
+            {
+                fprintf(stderr, "Transfer failed: %d\n", error);
+                error = 0;
+            }
+            else
+            {
+                /* if no error flag button as pressed */
+                trigger->active = 1;
+                /* increment index */
+                seq->i = (seq->i < seq->len) ? seq->i + 1 : 0;
+            }
+        }
+    }
+    else
+    {
+        trigger->active = 0;        // flag button as unpressed
+    }
+}
+
+void ShowInput()
+{
+    printf("\e[1;1H\e[2J");     // clear CLI
+    printf("Buttons:\n\t");
+
+    for (int i=0; i<2; i++)
+    {
+        for (int j=0; j<button_inputs[i]->number_of_bits; j++)
+        {
+            if ((INPUT[button_inputs[i]->input_index] & (1<<button_inputs[i]->relevant_bits[j])) != 0)
+            {
+                printf("%s, ", button_inputs[i]->buttons[j]);
+            }
+        }
+    }
+    printf("\n");
+
+    printf("Triggers & sticks\n");
+    printf("\tLeft trigger: \t%d\n", INPUT[4]);
+    printf("\tRight trigger: \t%d\n", INPUT[5]);
+    printf("\tLeft stick x: \t%d\n", INPUT[6]);
+    printf("\tLeft stick y: \t%d\n", INPUT[8]);
+    printf("\tRight stick x: \t%d\n", INPUT[10]);
+    printf("\tRight stick y: \t%d\n", INPUT[12]);
 }
---
+++ week3/assignment_f/gamepad.h
@@ -0,0 +1,104 @@
+#define IN  0x81    // message type for INPUT
+#define OUT 0x02    // message type for OUTPUT
+
+/*###################################################################
+                Reading input from the device 
+###################################################################*/
+/*
+ * The command to read input from the device
+ * The result is stored in the same array
+ */
+unsigned char INPUT[] = {0, 34, 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
+
+/*
+ * Struct to map the bits of bytes corresponding to buttons 
+ */
+typedef struct {
+    int input_index;        // index of this byte in the input array
+    int number_of_bits;     // number of relevant bits
+    int relevant_bits[8];   // positions of the relevant bits
+    char* buttons[8];       // Button names corresponding to relevant_bits by index
+} INPUT_STRUCT; 
+
+INPUT_STRUCT input_two = {
+    2, 8, {0, 1, 2, 3, 4, 5, 6, 7},
+    {"DPAD UP", "DPAD DOWN", "DPAD LEFT", "DPAF RIGHT", 
+    "START", "BACK", "L stick", "R stick"}
+};
+INPUT_STRUCT input_three = {
+    3, 7, {0, 1, 2, 4, 5, 6, 7}, 
+    {"LB", "RB", "XBOX", "A", "B", "X", "Y"}
+}; 
+
+/*
+ * Array for easy iteration over all buttons
+ */
+INPUT_STRUCT* button_inputs[2] = {&input_two, &input_three};
+
+
+/*###################################################################
+        Triggering actions based on buttons pressed 
+###################################################################*/
+/*
+ * Stuct for setting up triggers
+ */
+typedef struct {
+    int index;              // index of byte in the input array
+    int bit;                // bit for trigger in the input byte
+    int active;             // only trigger once per press
+} TRIGGER;
+
+/* rumble is triggered on button A input[3] bit 4 */
+TRIGGER trig_rmbl   = {3, 4, 0};
+/* rumble is triggered on button B input[3] bit 5 */
+TRIGGER trig_led    = {3, 5, 0};
+
+
+/*###################################################################
+            Commands to be executed in a sequences
+###################################################################*/
+/*
+ * Commands
+ */
+unsigned char RUMBLE_HIGH[]     = { 0, 8, 0, 0xff, 0xff, 0, 0, 0 };
+unsigned char RUMBLE_MID[]      = { 0, 8, 0, 0x88, 0x88, 0, 0, 0 };
+unsigned char RUMBLE_OFF[]      = { 0, 8, 0, 0x00, 0x00, 0, 0, 0 };
+
+unsigned char LED_ALL_OFF[]     = {0x01, 0x03, 0x00};
+unsigned char LED_ALL_ON[]      = {0x01, 0x03, 0x01};
+unsigned char LED_ON_ONE[]      = {0x01, 0x03, 0x06};
+unsigned char LED_ON_TWO[]      = {0x01, 0x03, 0x07};
+unsigned char LED_ON_THREE[]    = {0x01, 0x03, 0x08};
+unsigned char LED_ON_FOUR[]     = {0x01, 0x03, 0x09};
+
+/*
+ * Struct to store sequences
+ */
+typedef struct
+{
+    int cmd_len;            // length of command
+    int len;                // length of sequence
+    int i;                  // index
+    unsigned char* cmds[];  // sequence
+} SEQUENCE;
+
+SEQUENCE RUMBLE = {8, 1, 0, {RUMBLE_OFF, RUMBLE_MID, RUMBLE_HIGH}};
+SEQUENCE LED    = {3, 5, 0, {LED_ALL_ON, LED_ON_ONE, LED_ON_TWO,
+                    LED_ON_THREE, LED_ON_FOUR, LED_ALL_OFF}
+};
+
+/*###################################################################
+                    Function declarations
+###################################################################*/
+
+/*
+ * Checks inputs for any triggers, and executes sequence for trigger
+ */
+void HandleTrigger(libusb_device_handle* h, TRIGGER* trigger, SEQUENCE* seq)
+
+/*
+ * Print all buttons being pressed to the Cli
+ */
+void ShowInput();
+
+/*###################################################################*/
