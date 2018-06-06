03c776df62af54163375b21b64e5108ba24bf8c9
joshbindels <josh.bindels@gmail.com>
2018-06-06 13:58:16 +0200

    Removed assignment todo

--- week5/assignment_k/TCPEchoServer-Fork.c
+++ week5/assignment_k/TCPEchoServer-Fork.c
@@ -26,23 +26,6 @@ int main(int argc, char *argv[])
 
         clntSock = AcceptTCPConnection (servSock);
 
-        // TODO: write the code to realise the following:
-        //
-        // fork() a new process and:
-        // * check if the fork() gave problems
-        //   in this case, you must end the process (fatal error...)
-        // * check if you are the child process
-        //   in this case you have to:
-        //   - handle the communication with the client (use HandleTCPClient())
-        //   - and end the process
-        // * check if you are the parent
-        //   in this case you have to:
-        //   - wait for the next client (again in the while-loop)
-		//
-		// make sure that clntSock and servSock are closed at the correct locations 
-        // (in particular: at those places where you don't need them any more)
-		//
-		// Hint: use the info(), info_d(), info_d() operations to trace what happens
         processID = fork();
         info_d("%d Forked\n", processID);
 
